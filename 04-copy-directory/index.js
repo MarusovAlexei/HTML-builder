09e2e8682b8c4c03ba334adbd3f7bbd67a01c33f
Alexei Marusov <88613624+MarusovAlexei@users.noreply.github.com>
2022-05-24 02:53:25 +0300

    feat: add task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+// модуль чтения файлов
+const fs = require('fs');
 
+// модуль работы с путями к файлам
+const path = require('path');
+
+// старый и новый файлы
+const oldPath = path.join(__dirname, 'files');
+const newPath = path.join(__dirname, 'files-copy');
+
+// асинхронно создаем каталог
+fs.mkdir(newPath, {
+  recursive: true,
+}, (error) => {
+
+  if (error) throw error;
+});
+
+// читаем содержимое нового каталога
+fs.readdir(newPath, (error, files) => {
+
+  for (let i = 0; i < files.length; i++) {
+
+    // принимает имя файла, который нужно удалить
+    fs.unlink(newPath + '/' + files[i], error => {
+      if (error) throw error;
+    });
+  }
+});
+
+// читаем содержимое старого каталога
+fs.readdir(oldPath, (error, files) => {
+
+  if (error) throw error;
+
+  for (let i = 0; i < files.length; i++) {
+
+    // асинхронно копирует 'src' к 'dest'
+    fs.copyFile(oldPath + '/' + files[i], newPath + '/' + files[i], (error) => {
+      if (error) throw error;
+    });
+  }
+});
